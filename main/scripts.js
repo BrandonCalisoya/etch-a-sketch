9afb943f93c73826d67978fac615ca31412904f6
brandonc1 <ludsad07@gmail.com>
2025-01-27 12:30:23 -0400

    base

---
+++ main/scripts.js
@@ -0,0 +1,15 @@
+const dc = document.createElement()
+
+const main = document.createElement("main");
+
+
+const h1Main= document.createElement("h1");
+
+h1Main.textContent = "kiubo";
+
+main.appendChild(h1Main);
+
+
+
+document.body.appendChild(main);
+ 
